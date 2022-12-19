adfe08efeab3389b8a2dfb2d71c86ae037b3c36b
Your Name <4283301@myuwc.ac.za>
2022-12-19 12:15:02 +0200

    added typewriter js

--- LandingPageScript.js
+++ LandingPageScript.js
@@ -15,3 +15,56 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+const textDisplay = document.getElementById('text')
+const phrases = [ ' I am  a : ' , 'UX / UI Developer ' , '  Graphic Designer , ' , '  and Photographer . ']
+
+let i = 0
+let j = 0
+let currentPhrase = []
+let isDeleting =false
+let isEND = false
+
+function loop ( ) {
+  isEND = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+if (i < phrases.length ) {
+  
+  if( ! isDeleting && j <= phrases[i].length) {
+    currentPhrase.push(phrases [i] [j])
+    j++
+    
+  }
+
+  if (isDeleting && j<= phrases[i].length){
+    currentPhrase.pop(phrases[i][j])
+    j--
+    
+  }
+
+  if (j == phrases [i] . length) {
+    isEND =true
+   isDeleting=true
+  }
+
+  if ( isDeleting && j === 0){
+    currentPhrase = []
+    isDeleting = false
+    i++
+
+    if (i == phrases.length) {
+      i = 0
+    }
+  }
+
+  const spedUp = Math.random() * ( 80-50) +50
+   const normalSpeed = Math.random() * (300 -200) + 200
+   const deleteSpeed = Math.random() *(80-50) +50
+   const time = isEND ? 1500 : isDeleting ? spedUp : normalSpeed
+   setTimeout (loop, time)
+}
+
+
+}
+loop ( )
